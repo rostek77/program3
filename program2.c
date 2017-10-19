59811f513b21b2c815fda63174358169a76a97df
rostek77 <rostek77@o2.pl>
2017-10-19 17:59:30 +0200

    program3.c

---
+++ program2.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+
+
+int main()  
+{
+
+
+		putchar('V'); /*wpisz znak*/
+		putchar('\n'); /*znak konca lini*/
+		puts("Wpisz string/ ciag znakow");
+
+		puts("Ala ma kota, a kot ma Ale");
+	
+		return 0; 
+}
+
+
+
